fe57d22723582d2c2eb791b30cf8e5fb95d98571
alfindwi <alvindwi146@gmail.com>
2025-08-27 20:39:56 +0700

    refactor: simplify base64 encoding in CloudinaryService

--- src/services/cloudinary-service.ts
+++ src/services/cloudinary-service.ts
@@ -9,7 +9,7 @@ class CloudinaryService {
     });
   }
   async uploadSingle(file: Express.Multer.File) {
-    const b64 = Buffer.from(file.buffer).toString("base64");
+    const b64 = file.buffer.toString("base64");
     const dataURI = "data:" + file.mimetype + ";base64," + b64;
 
     return await cloudinary.uploader.upload(dataURI, {
